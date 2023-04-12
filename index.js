b4399a4412c014b31c3bfd3c3341b9628537e36e
Emmanuel Then <emmanuelthen@macbook-pro.mynetworksettings.com>
2023-04-11 23:47:24 -0400

    fixed hamburger on mobile screens, changed about me section more organized, fixed up modal pop up

--- index.js
+++ index.js
@@ -1,47 +1,3 @@
-// Theme will display when page is loaded based on users system settings (dark or light)
-document.addEventListener('DOMContentLoaded', () => {
-    const darkModeSystemSettings = () => {
-      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        // Set dark mode on
-        document.body.classList.add('dark-mode');
-      } else {
-        // Set dark mode off
-        document.body.classList.remove('dark-mode');
-      }
-    }
-    // Call the function to set initial dark mode based on system settings
-    darkModeSystemSettings();
-  });
-  
-
-
-
-// Made an array for each arrow at the end of the sections and  each section ID
-const arrowDownIds = ['arrow-down-1', 'arrow-down-2', 'arrow-down-3'];
-const nextSectionIds = ['section-1', 'section-2', 'section-3'];
-
-// Loops through the arrays and adds event listeners to each arrow
-arrowDownIds.forEach((id, index) => {
-  const arrow = document.getElementById(id);
-  const section = document.getElementById(nextSectionIds[index]);
-  arrow.addEventListener('click', function() {
-    section.scrollIntoView({ behavior: 'smooth' });
-  });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   //Dark-mode
   const toggleBtn = document.querySelector('.input');
   const body = document.querySelector('body');
@@ -74,6 +30,93 @@ arrowDownIds.forEach((id, index) => {
     }
   });
 
+//hamburger transform into an 'X' and drops menu when clicked 
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('open');
+  menu.classList.toggle('show-menu');
+});
+
+
+
+
+
+
+
+
+
+// Theme will display when page is loaded based on users system settings (dark or light)
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeSystemSettings = () => {
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // Set dark mode on
+        document.body.classList.add('dark-mode');
+        toggleBtn.checked = true;
+      } else {
+        // Set dark mode off
+        document.body.classList.remove('dark-mode');
+        toggleBtn.checked = false;
+      }
+    }
+    // Call the function to set initial dark mode based on system settings
+    darkModeSystemSettings();
+  });
+  
+
+
+
+// Made an array for each arrow at the end of the sections and  each section ID
+const arrowDownIds = ['arrow-down-1', 'arrow-down-2', 'arrow-down-3'];
+const navDownIds = ['nav-down-1', 'nav-down-2', 'nav-down-3']
+const nextSectionIds = ['section-1', 'section-2', 'section-3'];
+
+// Loops through the arrays and adds event listeners to each arrow
+arrowDownIds.forEach((id, index) => {
+  const arrow = document.getElementById(id);
+  const section = document.getElementById(nextSectionIds[index]);
+
+  arrow.addEventListener('click', () => {
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+navDownIds.forEach((id, index) => {
+    const nav = document.getElementById(id);
+    const section = document.getElementById(nextSectionIds[index]);
+
+    nav.addEventListener('click', () => {
+      section.scrollIntoView({behavior: 'smooth'});
+    })
+})
+
+//Lets-connect when it is clicked opens a new window for user to send a message and send it to my email
+const modal = document.querySelector('.modal');
+const connectButtonBottom = document.getElementById('connect-button');
+const connectButtonTop = document.getElementById('connect-button-top');
+const btnCloseModal = document.querySelector('.close-btn');
+const cancelBtn = document.querySelector('.btn-cancel');
+
+
+connectButtonBottom.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+connectButtonTop.addEventListener('click', () => {
+  modal.style.display = 'block'
+})
+
+btnCloseModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+
+
+
+
+
+
 
 
 
@@ -101,32 +144,8 @@ const hiddenElements2 = document.querySelectorAll('.hidden2');
 hiddenElements2.forEach((element) => observer.observe(element));
 
 
-//Burger menu on click
 
 
 
-
-//
-
-
-
-//Lets-connect when it is clicked opens a new window for user to send a message and send it to my email
-const modal = document.querySelector('.modal');
-const btnOpenModal = document.getElementById('connect-button');
-const btnCloseModal = document.querySelector('.close-btn');
-const cancelBtn = document.querySelector('.btn-cancel');
-
-
-btnOpenModal.addEventListener('click', () => {
-  modal.style.display = 'block';
-});
-
-btnCloseModal.addEventListener('click', () => {
-    modal.style.display = 'none';
-  });
-
-cancelBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
 
 
